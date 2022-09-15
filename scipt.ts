5784ffce2409402334508a05a9c03295340b793f
Sachin duhan <38427564+sachin-duhan26@users.noreply.github.com>
2022-09-15 23:27:13 +0530

    added more types and methodos

--- scipt.ts
+++ scipt.ts
@@ -1,4 +1,11 @@
-const vars : boolean = false;
+/*
+  int 
+  boolan
+  string
+  unknown 
+  any
+  undefined
+*/
 
 const another: string = 'string'; 
 
@@ -7,36 +14,79 @@ const age: number = 12;
 let something: string = 'pra nai kta';
 something = 'ab pta chal gya';
 
-let bloodGroup: 'A' | 'B' | 'AB' | 'O' = 'A';
+type Bloodgroup = 'A' | 'B' | 'AB' | 'O';
+let bloodGroup: Bloodgroup = 'A';
 
 // array?
 const marks: Array<number> = [];
 const votes: number[] = [];
 
+type NumOrString = number | string;
 
-type Student = {
-  name: string,
-  age: number,
-  license?: string
-}
+const meaning: typeof marks = [];
 
+// type Student = {
+//   name: string,
+//   age: number,
+//   license?: string
+// }
 
 // non premitive data type
-type Human = {
+interface Student {
   name: string,
   age: number,
   id_card: string | number
 }
 
-const __humans: Student[] = [
-  {
-    name: 'sachin',
-    age: 12,
-    license: '12182012',
-  },
-  {
-    name: 'sachin',
-    age: 12,
-  },
-]
+interface President extends Student {
+  club: string,
+}
+
+const sachin: President = {
+  age: 2,
+  id_card: 'as',
+  name: 'sachin',
+  club: 'pta nai'
+}
+
+// function getSalary(efforts: number): number {
+
+// }
+
+// const makeMoney = (efforts: number) :number => {
+  
+// }
+
+
+// interface 
+/**
+ * we define the attirbutes and their names in advance and not the actual functionality
+ * 
+*/
+
+let data: any = {};
+
+// const __humans: Student[] = [
+//   {
+//     name: 'sachin',
+//     age: 12,
+//     license: '12182012',
+//   },
+//   {
+//     name: 'sachin',
+//     age: 12,
+//   },
+// ]
+
+let varia = 12;
+
+const lunch: string[] = ['roti', 'sbji'];
+
+
+// implicit and explicit
+
+// implicit - khud se 
+// explicit - we manually define. 
+
+
 
